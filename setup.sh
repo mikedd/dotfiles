eea0bc30a971a8a8e068781f31fcc7d7bbba284c
mike <mike@aureuscattus.org>
2020-12-01 20:49:39 -0600

    Add alacritty terminfo

--- setup.sh
+++ setup.sh
@@ -15,3 +15,6 @@ ln -s ${DIR}/zsh/.zshrc ${HOME}/.zshrc
 ln -s ${DIR}/zsh/.zshenv ${HOME}/.zshenv
 
 ln -s ${DIR}/vim/.vim/init.vim ${HOME}/.config/nvim/init.vim
+
+
+sudo tic -xs alacritty,alacritty-direct ${DIR}/alacritty/alacritty.info
