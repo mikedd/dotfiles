afd4db46773e2fec4e90d4bb6542844cc62905f1
mike <mike@aureuscattus.org>
2020-12-01 20:45:44 -0600

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Gleefully stolen from: https://stackoverflow.com/a/246128
+# DIR is the directory that contains this script - think of a better name for it 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sudo apt install -y zsh neovim tmux
+
+mkdir -p ${HOME}/.config/tmux
+mkdir -p ${HOME}/.config/nvim
+
+ln -s ${DIR}/tmux/.tmux.conf ${HOME}/.config/tmux/tmux.conf
+
+ln -s ${DIR}/zsh/.zshrc ${HOME}/.zshrc
+ln -s ${DIR}/zsh/.zshenv ${HOME}/.zshenv
+
+ln -s ${DIR}/vim/.vim/init.vim ${HOME}/.config/nvim/init.vim
