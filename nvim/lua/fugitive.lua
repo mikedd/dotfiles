8f4ce4f335a709f63d86e74bdd648e8f538fdef5
mike <mike@aureuscattus.org>
2021-03-16 22:18:41 -0500

    Use git commit - as instructed

--- nvim/lua/fugitive.lua
+++ nvim/lua/fugitive.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_set_keymap('n', '<leader>gs', ':Gstatus<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gd', ':Gdiff<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gc', ':Gcommit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gc', ':Git commit<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gb', ':Gblame<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gl', ':Glog<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gp', ':Git push<CR>', { noremap = true, silent = true })
