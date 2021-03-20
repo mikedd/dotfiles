b6c82c058f40c84cc86932c696d06def32d22358
mike <mike@aureuscattus.org>
2021-03-20 09:13:56 -0500

    Use gitsigns instead of gitgutter/signify

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,7 +12,12 @@ return require('packer').startup(function()
     use 'tpope/vim-fugitive'
     use 'tpope/vim-surround'
     -- use 'airblade/vim-gitgutter'
-    use 'mhinz/vim-signify'
+    -- use 'mhinz/vim-signify'
+    use {
+        'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+         config = function() require('gitsigns').setup() end
+    }
+
     use 'christoomey/vim-tmux-navigator'
     use 'tpope/vim-commentary'
     use 'vim-scripts/dbext.vim'
@@ -44,47 +49,8 @@ return require('packer').startup(function()
   -- -- Simple plugins can be specified as strings
   -- use '9mm/vim-closer'
 
-  -- -- Lazy loading:
-  -- -- Load on specific commands
-  -- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
-
-  -- -- Load on an autocommand event
-  -- use {'andymass/vim-matchup', event = 'VimEnter'}
-
-  -- -- Load on a combination of conditions: specific filetypes or commands
-  -- -- Also run code after load (see the "config" key)
-  -- use {
-  --   'w0rp/ale',
-  --   ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-  --   cmd = 'ALEEnable',
-  --   config = 'vim.cmd[[ALEEnable]]'
-  -- }
-
-  -- -- Plugins can have dependencies on other plugins
-  -- use {
-  --   'haorenW1025/completion-nvim',
-  --   opt = true,
-  --   requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
-  -- }
-
-  -- -- Plugins can also depend on rocks from luarocks.org:
-  -- use {
-  --   'my/supercoolplugin',
-  --   rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
-  -- }
-
-  -- -- You can specify rocks in isolation
-  -- use_rocks 'penlight'
-  -- use_rocks {'lua-resty-http', 'lpeg'}
-
-  -- -- Local plugins can be included
-  -- use '~/projects/personal/hover.nvim'
-
   -- -- Plugins can have post-install/update hooks
   -- use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-
-  -- -- Post-install/update hook with neovim command
-  -- use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
   -- -- Post-install/update hook with call of vimscript function with argument
   -- use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
@@ -95,10 +61,5 @@ return require('packer').startup(function()
   --   requires = {'kyazdani42/nvim-web-devicons'}
   -- }
 
-  -- -- Use dependency and run lua function after load
-  -- use {
-  --   'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-  --   config = function() require('gitsigns').setup() end
-  -- }
 
 end)
