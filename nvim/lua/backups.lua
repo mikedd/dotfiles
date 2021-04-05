fb8ebe517b9167c5694da00e822cb69dbeb5ade4
Michael Dick <michael.d.dick@gmail.com>
2021-04-05 17:23:51 -0500

    They're options not variables - Mike.

--- nvim/lua/backups.lua
+++ nvim/lua/backups.lua
@@ -1,6 +1,6 @@
 local base = os.getenv("HOME") .. "/.vim"
 
-vim.api.nvim_set_var('backupdir', base .. '/backup')
-vim.api.nvim_set_var('viewdir', base .. '/views')
-vim.api.nvim_set_var('directory', base .. '/swap')
-vim.api.nvim_set_var('undodir', base .. '/undo')
+vim.api.nvim_set_option('backupdir', base .. '/backup')
+vim.api.nvim_set_option('viewdir', base .. '/views')
+vim.api.nvim_set_option('directory', base .. '/swap')
+vim.api.nvim_set_option('undodir', base .. '/undo')
