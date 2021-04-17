226df3e86c7c395b83404b0ac2986be1e7408d82
mike <mike@aureuscattus.org>
2021-04-17 11:27:51 -0500

    Clean up mappings

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,15 +1,16 @@
 local set_keymap = vim.api.nvim_set_keymap
-local opts = { noremap=true }
+local opts = {noremap = true}
+local cmd_ts = '<cmd>lua require("telescope.builtin")'
 
-set_keymap('n', '<c-p>', [[<cmd>lua require('telescope.builtin').find_files({hidden = true})<cr>]], opts)
-set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files({hidden = true})<cr>]], opts)
-set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep({hidden = true})<cr>]], opts)
-set_keymap('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], opts)
-set_keymap('n', '<leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], opts)
-set_keymap('n', '<leader>fq', [[<cmd>lua require('telescope.builtin').quickfix()<cr>]], opts)
+set_keymap('n', '<c-p>', cmd_ts .. '.find_files({hidden = true})<cr>', opts)
+set_keymap('n', '<leader>ff', cmd_ts .. '.find_files({hidden = true})<cr>', opts)
+set_keymap('n', '<leader>fg', cmd_ts .. '.live_grep({hidden = true})<cr>', opts)
+set_keymap('n', '<leader>fb', cmd_ts .. '.buffers()<cr>', opts)
+set_keymap('n', '<leader>fh', cmd_ts .. '.help_tags()<cr>', opts)
+set_keymap('n', '<leader>fq', cmd_ts .. '.quickfix()<cr>', opts)
 
 -- lsp
-set_keymap('n', '<leader>fr', [[<cmd>lua require('telescope.builtin').lsp_references()<cr>]], opts)
-set_keymap('n', '<leader>fa', [[<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>]], opts)
-set_keymap('n', '<leader>fd', [[<cmd>lua require('telescope.builtin').lsp_document_diagnostics()<cr>]], opts)
-set_keymap('n', '<leader>fs', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>]], opts)
+set_keymap('n', '<leader>fr', cmd_ts .. '.lsp_references()<cr>', opts)
+set_keymap('n', '<leader>fa', cmd_ts .. '.lsp_code_actions()<cr>', opts)
+set_keymap('n', '<leader>fd', cmd_ts .. '.lsp_document_diagnostics()<cr>', opts)
+set_keymap('n', '<leader>fs', cmd_ts .. '.lsp_document_symbols()<cr>', opts)
