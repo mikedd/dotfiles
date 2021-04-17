b3ea37857ac05a1e79eb68ee8b854801162d252d
mike <mike@aureuscattus.org>
2021-04-17 11:26:54 -0500

    Add mapping to remove trailing whitespace

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+local set_keymap = vim.api.nvim_set_keymap
+local opts = { noremap=true }
 local cmd = vim.api.nvim_command
 cmd("syntax on")
 cmd("filetype plugin indent on")
@@ -6,19 +8,8 @@ require "plugins"
 require "options"
 require "config"
 
--- require "/home/mike/.dbext.lua"
--- " DBExt
--- source $HOME/.psql.vim
 
---     " Instead of reverting the cursor to the last position in the buffer, we
---     " set it to the first line when editing a git commit message
---     au FileType gitcommit au! BufEnter COMMIT_EDITMSG call setpos('.', [0, 1, 1, 0]
-
---     " Filetypes
---     " gradle syntax highlighting
---     au BufNewFile,BufRead *.gradle set filetype=groovy
---     " markdown  highlighting
---     au BufNewFile,BufRead *.md set filetype=markdown
+set_keymap('n', '<F5>', [[<cmd>:let _s=@/<Bar>:%s/\s\+$//e<Bar>:let @/=_s<Bar>:nohl<CR>]], opts)
 
     -- " Remove trailing whitespace
     -- autocmd FileType c,cpp,javscript autocmd BufWritePre <buffer> :%s/\s\+$//e"
