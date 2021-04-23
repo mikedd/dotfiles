da380e8f93c1d033c7dedaa2159825f772eb9e23
mike <mike@aureuscattus.org>
2021-04-23 11:45:53 -0500

    Add more telescope commands

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -14,3 +14,11 @@ set_keymap('n', '<leader>fr', cmd_ts .. '.lsp_references()<cr>', opts)
 set_keymap('n', '<leader>fa', cmd_ts .. '.lsp_code_actions()<cr>', opts)
 set_keymap('n', '<leader>fd', cmd_ts .. '.lsp_document_diagnostics()<cr>', opts)
 set_keymap('n', '<leader>fs', cmd_ts .. '.lsp_document_symbols()<cr>', opts)
+set_keymap('n', '<leader>fw', cmd_ts .. '.lsp_workspace_symbols()<cr>', opts)
+set_keymap('n', '<leader>fe', cmd_ts .. '.lsp_workspace_diagnostics()<cr>', opts)
+set_keymap('n', '<leader>fD', cmd_ts .. '.lsp_definitions()<cr>', opts)
+
+-- git
+set_keymap('n', '<leader>fh', cmd_ts .. '.git_status()<cr>', opts)
+set_keymap('n', '<leader>fj', cmd_ts .. '.git_branches()<cr>', opts)
+set_keymap('n', '<leader>fc', cmd_ts .. '.git_commits()<cr>', opts)
