12ff14828b65bb8ca45a142ab92df2166c244bf0
mike <mike@aureuscattus.org>
2021-05-09 10:35:28 -0500

    Add colorizer

---
+++ nvim/lua/config/nvim-colorizer.lua
@@ -0,0 +1,14 @@
+require'colorizer'.setup {
+    -- TODO for enable for all files
+    -- to restrict
+    --
+    -- Attach to certain Filetypes, add special configuration for `html`
+    -- Use `background` for everything else.
+    -- require 'colorizer'.setup {
+    --   'css';
+    --   'javascript';
+    --   html = {
+    --     mode = 'foreground';
+    --   }
+    -- }
+}
